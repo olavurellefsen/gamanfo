73cd54148d6e5bd865f4053f709dcaacbfbe193c
olavurellefsen <oe@tokni.com>
2019-12-03 23:12:10 +0000

    Remove header

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,25 +7,13 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import '../styles/styles.scss'
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
       <div>
         <main>{children}</main>
       </div>
