325904b2b16605f633f467c780a1790aeac70003
olavurellefsen <oe@tokni.com>
2019-12-05 07:21:32 +0000

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gaman-games-logo.webp`, // This path is relative to the root of the site.
+        icon: `src/images/gaman-games-logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
